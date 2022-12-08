aed694adc0089bc5c2175e40f8d43c355c665c88
kdkdhoho <hkim4410@naver.com>
2022-12-08 10:24:53 +0900

    refactor(LottoController): run 메서드 나눔

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -13,12 +13,24 @@ public class LottoController {
     private final LottoService lottoService = new LottoService();
 
     public void run() {
+        setPlayer();
+        setManager();
+        printTotalResult();
+    }
+
+    private void setPlayer() {
         String money = inputView.readMoney();
         lottoService.setPlayer(money);
         outputView.printPurchase(lottoService.getPlayerLottoNumbers());
+    }
+
+    private void setManager() {
         String winningNumbers = inputView.readWinningNumbers();
         String bonusNumber = inputView.readBonusNumber();
         lottoService.setManager(winningNumbers, bonusNumber);
+    }
+
+    private void printTotalResult() {
         Map<Rank, Integer> ranks = lottoService.getResult();
         int playerMoney = lottoService.getPlayerMoney();
         outputView.printTotalResult(ranks, playerMoney);
